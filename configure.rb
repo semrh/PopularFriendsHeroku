9de4d7835b10c8281d2f2b6fadded5deee96ca51
Sem Ramos <alu0100603194@ull.edu.es>
2014-10-01 21:29:26 +0000

    Añadiendo configure.rb bien configurado

---
+++ configure.rb
@@ -0,0 +1,8 @@
+def my_twitter_client
+  Twitter::REST::Client.new do |config|
+    config.consumer_key       = 'z8Qtk8oBgM2ykx8igIQa5Dbjq'
+    config.consumer_secret    = 'Ma0mfqGh1iF17vEe6P9lArtwo8Ik8MpZnO5gCjCyYSyLtiN7Ve'
+    config.oauth_token        = "85652810-2hBxsypy8WF6Cadun46cunplIpAhGksndzGRVK1gU"
+    config.oauth_token_secret = "CKldAFFAQc0v7ssVS4RTd7gbcV3FcwHJsZFePCWwi1duJ"
+  end
+end
